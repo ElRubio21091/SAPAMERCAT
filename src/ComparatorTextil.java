8e5d9ad9d295e62f04fd6242a026cadecda47df7
Raul <21raulyyo@gmail.com>
2026-03-31 15:35:23 +0200

    Comparator de textils

---
+++ src/ComparatorTextil.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class ComparatorTextil implements Comparator<Textil> {
+
+    @Override
+    public int compare(Textil t1, Textil t2) {
+        return t1.getComposicio().compareTo(t2.getComposicio());
+    }
+}
